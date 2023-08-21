69ee5ee7caf223080b5d3ab66196f1de1a7848f5
Jair Gomez <jairherber505@gmail.com>
2023-08-21 13:47:29 -0700

    back-end progress

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,9 +3,20 @@ const { User } = require("../models/User")
 const { signToken } = require("../utils/auth")
 
 const resolvers = {
+    // ? Is this correct?
     Query: {
-        user: async () => {
-            return 
+        users: async () => {
+            return User.find()
+        },
+        user: async (parent, { userId }) => {
+            return User.findOne({ _id: userId })
+        },
+
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id})
+            }
+            throw new AuthenticationError("Login sir.")
         }
     }
 }
